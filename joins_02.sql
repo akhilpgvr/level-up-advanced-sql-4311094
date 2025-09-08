25e661c49e22eb07f66fb3d3944abfb8692b9ca8
Akhil P. S. <49089198+akhilpgvr@users.noreply.github.com>
2025-09-08 05:47:30 +0000

    joins question 02

--- joins_02.sql
+++ joins_02.sql
@@ -1,2 +1,6 @@
 select * from employee;
 select * from sales;
+
+select e.employeeId, e.firstName, e.lastName, s.salesId from employee e
+left join sales s on e.employeeId=s.employeeId
+where e.title='Sales Person' and s.salesId IS NULL;
